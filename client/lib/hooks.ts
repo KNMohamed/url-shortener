68296057296ffe5169ae9596b3d06ec5e6e460d9
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2025-08-22 12:33:38 -0400

    Fix useSelector typing

--- client/lib/hooks.ts
+++ client/lib/hooks.ts
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
 import type { AppStore, AppDispatch } from './store'
+import type { RootState } from './features/types'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<AppStore> = useSelector
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
