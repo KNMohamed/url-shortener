be1d3bd576336315310d9d82d1baea012242e83c
Khalid Mohamed <khalid.n.mohamed@outlook.com>
2025-08-25 01:27:27 -0400

    Fix config file

--- server/config.js
+++ server/config.js
@@ -1,13 +1,13 @@
 export default {
   PORT: 3001,
 
-  /* The domain that this website is on */
+  /* The domain for the backend is on */
   DEFAULT_DOMAIN: "localhost:3001",
 
   /* Neo4j database credential details */
   DB_URI: "neo4j://127.0.0.1:7687",
   DB_USERNAME: "neo4j",
-  DB_PASSWORD: "3nYi3AIni9GPuivDY7wABL5OkEd7pr8zYmkbnFI6FUI",
+  DB_PASSWORD: "YOUR_PASSWORD",
 
   /* A passphrase to encrypt JWT. Use a long and secure key. */
   JWT_SECRET: "securekey",
